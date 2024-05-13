279e14553c8a93c921eac08c80a89c46be9d8bf5
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-05-13 18:48:16 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
 
 import Description  from './components/description/description';
+import Options from './components/options/FeedbackOptions';
 
-export default function App (){
-return(
 
+
+  class App extends Component {
+    state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  handleButtonClick = option => {
+    this.setState({
+      [option]: this.state[option] + 1,
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
+  render(){
+    const { good, neutral, bad } = this.state;
+return (
+<>
 
   < Description/>
-   
-)
-}
+  <Options
+ options={Object.keys(this.state)}
+ onButtonClick={this.handleButtonClick}
+/> 
+
+          
+          
+   </>
+    );
+  }}
+  
+
+
+export default App;
 
 
 
--- src/components/options/FeedbackOptions.jsx
+++ src/components/options/FeedbackOptions.jsx
@@ -1,25 +1,29 @@
-import PropTypes from 'prop-types';
-import { Button } from './FeedbackOptionsStyles';
 
-const FeedbackOptions = ({ options, onButtonClick }) => {
-  return (
-    <div>
-      {options.map(option => (
-        <Button
-          type="button"
-          key={option}
-          onClick={() => onButtonClick(option)}
-        >
-          {option}
-        </Button>
-      ))}
-    </div>
-  );
-};
 
-FeedbackOptions.propTypes = {
-  options: PropTypes.array.isRequired,
-  onButtonClick: PropTypes.func.isRequired,
-};
-
-export default FeedbackOptions;
+export default function Options(){ 
+  import PropTypes from 'prop-types';
+  import { Button } from './FeedbackOptionsStyles';
+  
+  const FeedbackOptions = ({ options, onButtonClick }) => {
+    return (
+      <div>
+        {options.map(option => (
+          <Button
+            type="button"
+            key={option}
+            onClick={() => onButtonClick(option)}
+          >
+            {option}
+          </Button>
+        ))}
+      </div>
+    );
+  };
+  
+  FeedbackOptions.propTypes = {
+    options: PropTypes.array.isRequired,
+    onButtonClick: PropTypes.func.isRequired,
+  };
+}
+  
+  
